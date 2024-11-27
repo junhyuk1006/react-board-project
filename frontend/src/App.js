bd43831852c2db354e7e3ef037c97210b32b7d33
최준혁 <whdid3766@gmail.com>
2024-11-27 18:28:28 +0900

    백 프론트 연결 테스트

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import BoardList from './components/BoardList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React 게시판</h1>
+          <BoardList/>
     </div>
   );
 }
